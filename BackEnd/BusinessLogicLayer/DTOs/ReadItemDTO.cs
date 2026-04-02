8a814f3e4c4d93ce9be1fcfc9f603e3d6682337d
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-03 02:01:13 +0300

    Make method for getting all read items

--- BackEnd/BusinessLogicLayer/DTOs/ReadItemDTO.cs
+++ BackEnd/BusinessLogicLayer/DTOs/ReadItemDTO.cs
@@ -19,6 +19,13 @@ namespace BusinessLogicLayer.DTOs
             Type = type;
         }
 
+        public ReadItemDTO(ReadItem readItem)
+            : this(readItem.Id,
+                   readItem.Title,
+                   Enum.Parse<ReadItemType>(readItem.Type, ignoreCase: true))
+        {
+        }
+
         public ReadItem ToReadItem() => new ReadItem
         {
             Id = this.Id,
