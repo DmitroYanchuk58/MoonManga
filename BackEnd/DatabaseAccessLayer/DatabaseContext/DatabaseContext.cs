4ea70b501d8157bdb358aeae54b4187f7d430b18
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-02 15:05:37 +0300

    Make database context

---
+++ BackEnd/DatabaseAccessLayer/DatabaseContext/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using DatabaseAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAccessLayer.DatabaseContext
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<ReadItem> ReadItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ReadItem>().ToTable("ReadItems");
+
+            modelBuilder.Entity<ReadItem>()
+                .HasKey(b => b.Id);
+
+            modelBuilder.Entity<ReadItem>()
+            .Property(b => b.Title)
+            .HasMaxLength(100)
+            .IsRequired();
+
+            modelBuilder.Entity<ReadItem>()
+                .Property(b => b.Type)
+                .IsRequired();
+        }
+    }
+}
