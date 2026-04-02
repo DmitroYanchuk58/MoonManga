c2a5ade16824af31e69e55644b5baece15a0b335
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-02 15:36:45 +0300

    Add migrations and update database

--- BackEnd/Catalog/Program.cs
+++ BackEnd/Catalog/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<DatabaseContext>(options =>
+builder.Services.AddDbContext<CatalogDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CatalogDB")));
 
 var app = builder.Build();
