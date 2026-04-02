77f2289a6367f745898a0a886a86539405070435
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-03 00:38:00 +0300

    Make test for creating read item

---
+++ BackEnd/BusinessLogicLayer/Services/ReadItemService.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLogicLayer.DTOs;
+using DatabaseAccessLayer.DatabaseContext;
+using DatabaseAccessLayer.Entities;
+using DatabaseAccessLayer.Repositories;
+
+namespace BusinessLogicLayer.Services
+{
+    public class ReadItemService
+    {
+        private ICRUD<ReadItem> _repository;
+        public ReadItemService(CatalogDBContext context)
+        {
+            _repository = new CrudRepository<ReadItem>(context);
+        }
+
+        public async Task CreateReadItemAsync(ReadItemDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ReadItemDTO> GetReadItemByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ReadItemDTO>> GetAllReadItemsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateReadItemAsync(ReadItemDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteReadItemAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BackEnd/BusinessLogicLayer/Validation/ReadItemValidator.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLogicLayer.DTOs;
+using FluentValidation;
+
+namespace BusinessLogicLayer.Validation
+{
+    public class ReadItemValidator : AbstractValidator<ReadItemDTO>
+    {
+        public ReadItemValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Please write title").Length(1,100).WithMessage("Title should has minimum 1 symbol length and 100 symbol length");
+            RuleFor(x => x.Type).NotEmpty().WithMessage("Please choose type of item");
+        }
+    }
+}
