30f78638befb6edd7a699625893c04ee369768a8
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-02 15:52:40 +0300

    Add interface for crud operations classes

---
+++ BackEnd/DatabaseAccessLayer/Repositories/ICRUD.cs
@@ -0,0 +1,21 @@
+﻿using DatabaseAccessLayer.Entities;
+
+namespace DatabaseAccessLayer.Repositories
+{
+    public interface ICRUD<T> where T : Entity
+    {
+        public Task CreateAsync(T entity);
+
+        // READ (Один за ID)
+        public Task<T> GetByIdAsync(Guid id);
+
+        // READ (Всі записи)
+        public Task<List<T>> GetAllAsync();
+
+        // UPDATE
+        public Task UpdateAsync(T entity);
+
+        // DELETE
+        public Task DeleteAsync(Guid id);
+    }
+}
