8f14e805d8eb2141af0b6b58099b525b02425414
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-03 00:04:44 +0300

    Expand DTO class with constuctors and method converting into enity

--- BackEnd/BusinessLogicLayer/DTOs/ReadItemDTO.cs
+++ BackEnd/BusinessLogicLayer/DTOs/ReadItemDTO.cs
@@ -1,11 +1,29 @@
-﻿namespace BusinessLogicLayer.DTOs
+﻿using DatabaseAccessLayer.Entities;
+
+namespace BusinessLogicLayer.DTOs
 {
     public class ReadItemDTO
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
 
         public string Title { get; set; }
 
         public ReadItemType Type { get; set; }
+
+        public ReadItemDTO() { }
+
+        public ReadItemDTO(Guid id, string title, ReadItemType type) : this()
+        {
+            Id = id;
+            Title = title;
+            Type = type;
+        }
+
+        public ReadItem ToReadItem() => new ReadItem
+        {
+            Id = this.Id,
+            Title = this.Title,
+            Type = this.Type.ToString()
+        };    
     }
 }
