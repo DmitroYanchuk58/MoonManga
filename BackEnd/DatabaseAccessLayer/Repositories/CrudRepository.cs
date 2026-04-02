e8a2ea890680fa2631c9e4f2d6ee5135caed9e87
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-02 15:54:16 +0300

    Make repository for crud operations

---
+++ BackEnd/DatabaseAccessLayer/Repositories/CrudRepository.cs
@@ -0,0 +1,55 @@
+﻿using DatabaseAccessLayer.DatabaseContext;
+using DatabaseAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAccessLayer.Repositories
+{
+    public class CrudRepository<T> : ICRUD<T> where T : Entity
+    {
+        private readonly CatalogDBContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public CrudRepository(CatalogDBContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        // CREATE
+        public async Task CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        // READ (Один за ID)
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        // READ (Всі записи)
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        // UPDATE
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        // DELETE
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
