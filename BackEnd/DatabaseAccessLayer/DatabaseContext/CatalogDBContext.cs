331533bf506e60caf1b129eab8e201e6a0e5c194
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-02 23:52:45 +0300

    Make tests for crud repository for read item

--- BackEnd/DatabaseAccessLayer/DatabaseContext/CatalogDBContext.cs
+++ BackEnd/DatabaseAccessLayer/DatabaseContext/CatalogDBContext.cs
@@ -13,6 +13,10 @@ namespace DatabaseAccessLayer.DatabaseContext
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //Project with test use sqlite, so we need to use LENGTH instead of LEN for check constraint
+            //All other project use SQL Server, so we need use LEN for check constraint
+            var lengthFunctionName = Database.IsSqlite() ? "LENGTH" : "LEN";
+
             modelBuilder.Entity<ReadItem>(entity =>
             {
                 entity.ToTable("ReadItems");
@@ -26,8 +30,8 @@ namespace DatabaseAccessLayer.DatabaseContext
                 entity.Property(b => b.Type)
                     .IsRequired();
 
-                entity.ToTable(t => t.HasCheckConstraint("CK_ReadItem_Title_NotEmpty", "LEN(TRIM(Title)) > 0"));
-                entity.ToTable(t => t.HasCheckConstraint("CK_ReadItem_Type_NotEmpty", "LEN(TRIM(Type)) > 0"));
+                entity.ToTable(t => t.HasCheckConstraint("CK_ReadItem_Title_NotEmpty", $"{lengthFunctionName}(TRIM(Title)) > 0"));
+                entity.ToTable(t => t.HasCheckConstraint("CK_ReadItem_Type_NotEmpty", $"{lengthFunctionName}(TRIM(Type)) > 0"));
             });
         }
     }
