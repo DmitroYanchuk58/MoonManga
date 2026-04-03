4f0f0ac604cab5dd515f25bc34c43fab68390239
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-03 23:29:52 +0300

    Make api requests for read item controller

--- BackEnd/Catalog/Controllers/ReadItemController.cs
+++ BackEnd/Catalog/Controllers/ReadItemController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -6,6 +8,46 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ReadItemController : ControllerBase
     {
+        private IReadItemService _service;
 
+        public ReadItemController(IReadItemService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetReadItems")]
+        public async Task<IActionResult> GetReadItems()
+        {
+            var readItems = await _service.GetAllReadItemsAsync();
+            return Ok(readItems);
+        }
+
+        [HttpGet("GetReadItem")]
+        public async Task<IActionResult> GetReadItem(Guid id)
+        {
+            var readItem = await _service.GetReadItemByIdAsync(id);
+            return Ok(readItem);
+        }
+
+        [HttpPost("CreateReadItem")]
+        public async Task<IActionResult> CreateReadItem([FromBody] ReadItemDTO item)
+        {
+            await _service.CreateReadItemAsync(item);
+            return Ok(item);
+        }
+
+        [HttpPut("UpdateReadItem")]
+        public async Task<IActionResult> UpdateReadItem([FromBody] ReadItemDTO item)
+        {
+            await _service.UpdateReadItemAsync(item);
+            return Ok();
+        }
+
+        [HttpDelete("DeleteReadItem")]
+        public async Task<IActionResult> DeleteReadItem(Guid id)
+        {
+            await _service.DeleteReadItemAsync(id);
+            return Ok();
+        }
     }
 }
