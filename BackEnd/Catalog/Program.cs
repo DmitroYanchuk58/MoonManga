40dce774075bb13c5dfa464fb1dc141f2210bb14
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-03 23:10:36 +0300

    Add swagger into api project

--- BackEnd/Catalog/Program.cs
+++ BackEnd/Catalog/Program.cs
@@ -1,7 +1,6 @@
 using BusinessLogicLayer.Services;
 using BusinessLogicLayer.Services.Interfaces;
 using DatabaseAccessLayer.DatabaseContext;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,10 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddDbContext<CatalogDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CatalogDB")));
 
-builder.Services.AddScoped<IReadItemService, ReadItemService>();
+
 
 var app = builder.Build();
 
@@ -22,6 +25,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
