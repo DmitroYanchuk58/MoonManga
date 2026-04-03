f62b5374e8d80befb03a7ce7f0731404078edfd8
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-04 00:18:50 +0300

    Make middleware for catching exceptions

---
+++ BackEnd/Catalog/Models/ErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace API.Models
+{
+    public class ErrorResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
