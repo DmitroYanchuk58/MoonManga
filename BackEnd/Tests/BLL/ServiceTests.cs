2ade34fdeaa4aa2fa7c373bb7e36c009202617b4
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-18 00:27:45 +0300

    Make tests for chapter service

---
+++ BackEnd/Tests/BLL/ServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using DatabaseAccessLayer.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests.BLL
+{
+    public class ServiceTests
+    {
+        protected CatalogDBContext GetDbContext()
+        {
+            var connection = new Microsoft.Data.Sqlite.SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<CatalogDBContext>()
+                .UseSqlite(connection)
+                .Options;
+            var context = new CatalogDBContext(options);
+
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+}
