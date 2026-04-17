ed62d9863cf8792bc356ede829428bd20b0c591c
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-17 16:22:01 +0300

    Make interface for chapter service

---
+++ BackEnd/BusinessLogicLayer/Services/Interfaces/IChapterService.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogicLayer.DTOs;
+
+namespace BusinessLogicLayer.Services.Interfaces
+{
+    public interface IChapterService
+    {
+        public Task CreateChapterAsync(ChapterDTO item);
+
+        public Task<ChapterDTO> GetChapterByIdAsync(Guid id);
+
+        public Task<List<ChapterDTO>> GetAllChaptersAsync();
+
+        public Task UpdateChapterAsync(ChapterDTO item);
+
+        public Task DeleteChapterAsync(Guid id);
+    }
+}
