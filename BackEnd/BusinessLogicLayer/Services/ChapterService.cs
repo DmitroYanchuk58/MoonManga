fc29fd3111a1153d08998f4e241f110b8eae1016
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-17 16:31:10 +0300

    Make validator for chapter DTO

---
+++ BackEnd/BusinessLogicLayer/Services/ChapterService.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Services.Interfaces;
+
+namespace BusinessLogicLayer.Services
+{
+    public class ChapterService : IChapterService
+    {
+        public Task CreateChapterAsync(ChapterDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteChapterAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ChapterDTO>> GetAllChaptersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ChapterDTO> GetChapterByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateChapterAsync(ChapterDTO item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BackEnd/BusinessLogicLayer/Validation/ChapterValidator.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogicLayer.DTOs;
+using FluentValidation;
+
+namespace BusinessLogicLayer.Validation
+{
+    public class ChapterValidator : AbstractValidator<ChapterDTO>
+    {
+        public ChapterValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Order)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .GreaterThan(0)
+                .WithMessage("Order must be a positive number starting from 1.");
+        }
+    }
+}
