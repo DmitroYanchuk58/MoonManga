5fa0d5e357274f1866baaecd800d4376dafc4229
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-23 18:17:43 +0300

    Make validator for page dto

---
+++ BackEnd/BusinessLogicLayer/Validation/PageValidator.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogicLayer.DTOs;
+using FluentValidation;
+
+namespace BusinessLogicLayer.Validation
+{
+    public class PageValidator : AbstractValidator<PageDTO>
+    {
+        public PageValidator() 
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Order)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .GreaterThan(0)
+                .WithMessage("Order must be a positive number starting from 1.");
+            RuleFor(x => x.Image)
+                .NotNull()
+                .WithMessage("Image data must not be null.")
+                .Must(image => image.Length > 0)
+                .WithMessage("Image data must not be empty.");
+        }
+    }
+}
