28c073f3230927872bce04dd2248e8bcbd59b87d
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-05-10 01:08:32 +0300

    Make Join repository with interface

---
+++ BackEnd/DatabaseAccessLayer/Repositories/Interfaces/IJoin.cs
@@ -0,0 +1,20 @@
+﻿using DatabaseAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccessLayer.Repositories.Interfaces
+{
+    public interface IJoin<TMain, TLink, TTarget>
+        where TMain : Entity
+        where TLink : Entity
+        where TTarget : Entity
+    {
+        public Task<List<TMain>> GetCombinedDataAsync(
+                    Expression<Func<TMain, IEnumerable<TLink>>> linkProperty,
+                    Expression<Func<TLink, TTarget>> targetProperty);
+    }
+}
---
+++ BackEnd/DatabaseAccessLayer/Repositories/JoinRepository.cs
@@ -0,0 +1,27 @@
+﻿using DatabaseAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+public class JoinRepository<TMain, TLink, TTarget>
+    where TMain : Entity
+    where TLink : Entity
+    where TTarget : Entity
+{
+    private readonly DbContext _context;
+
+    public JoinRepository(DbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<TMain>> GetCombinedDataAsync(
+        Expression<Func<TMain, IEnumerable<TLink>>> linkProperty,
+        Expression<Func<TLink, TTarget>> targetProperty)
+    {
+        return await _context.Set<TMain>()
+            .AsNoTracking()
+            .Include(linkProperty)
+                .ThenInclude(targetProperty)
+            .ToListAsync();
+    }
+}
