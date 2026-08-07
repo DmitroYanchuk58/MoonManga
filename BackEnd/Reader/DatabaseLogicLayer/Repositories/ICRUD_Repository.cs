a796522a20ca4bc33aeb53fc1c3391e299e1c88f
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-08-07 15:21:25 +0300

    Make crud repo for reader

---
+++ BackEnd/Reader/DatabaseLogicLayer/Repositories/ICRUD_Repository.cs
@@ -0,0 +1,20 @@
+﻿using DatabaseLogicLayer.Entities;
+using System.Linq.Expressions;
+
+namespace DatabaseLogicLayer.Repositories
+{
+    public interface ICRUD_Repository<T> where T : Entity
+    {
+        public Task CreateAsync(T entity);
+
+        public Task<T> GetByIdAsync(Guid id);
+
+        public Task<List<T>> GetAllAsync();
+
+        public Task UpdateAsync(T entity);
+
+        public Task DeleteAsync(Guid id);
+
+        public Task<List<T>> GetByConditionAsync(Expression<Func<T, bool>> predicate);
+    }
+}
