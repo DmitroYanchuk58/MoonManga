b80d3719bd9a414267fab52ecd9dbeb5951280e4
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-08-07 15:05:59 +0300

    Make empty class for dal tests

---
+++ BackEnd/Reader/Reader/Tests/DLL/PageRepo_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.DLL
+{
+    public class PageRepo_Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
